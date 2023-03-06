346aac4bd5ebdcd230338e4f99d0dfbb25420962
Alexey Lisov <lisov.alex@gmail.com>
2023-03-06 09:23:18 +0000

    Initial Commit

---
+++ StoreLab Coding Challenge/StoreLab_Coding_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreLab_Coding_ChallengeApp.swift
+//  StoreLab Coding Challenge
+//
+//  Created by Alexey Lisov on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct StoreLab_Coding_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
