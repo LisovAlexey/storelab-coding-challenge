bbe7801ab7886c6e83b1edf0ffe11d663582a47c
Alexey Lisov <lisov.alex@gmail.com>
2023-03-06 13:49:05 +0000

    Mock ImageInfo NukeUI package imported

--- StoreLab Coding Challenge/Core/Domain/Model/ImageInfo.swift
+++ StoreLab Coding Challenge/Core/Domain/Model/ImageInfo.swift
@@ -8,6 +8,13 @@
 import Foundation
 
 struct ImageInfo: Codable {
+    
+    static let mock = ImageInfo(
+        id: 220, author: "Robin Röcker", width: 3872, height: 2416,
+        url: URL(string: "https://unsplash.com/photos/qUToqliACNA")!,
+        downloadUrl: URL(string: "https://picsum.photos/id/220/200/200")!
+    )
+    
     var id: Int
     var author: String
     var width: Int
---
+++ StoreLab Coding Challenge/Core/Domain/Model/ImageInfoMock.swift
@@ -0,0 +1,8 @@
+//
+//  ImageInfoMock.swift
+//  StoreLab Coding Challenge
+//
+//  Created by Alexey Lisov on 06/03/2023.
+//
+
+import Foundation
